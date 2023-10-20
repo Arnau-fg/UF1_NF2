1f06a32553b4efc63d35e635129ee4154f6bf92d
Arnau-fg <a22arnfergil@inspedralbes.cat>
2023-10-20 13:02:07 +0200

    millorat codi

--- ComunicationManager.js
+++ ComunicationManager.js
@@ -1,9 +1,7 @@
 // http://www.omdbapi.com/?s=car&apikey=bb55fec8&page=1
 
 export async function agafarPelicules(pagina){
-    let data = "";
-
-    const response = await fetch(`data.JSON`)
+    const response = await fetch(`data.JSON`);
     const productes = await response.json();
 
     return productes;

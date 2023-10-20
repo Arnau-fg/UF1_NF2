a27ccd6d84136c854322fd175a9287298a0f10d9
Arnau-fg <a22arnfergil@inspedralbes.cat>
2023-10-20 12:37:13 +0200

    first commit

---
+++ ComunicationManager.js
@@ -0,0 +1,12 @@
+// http://www.omdbapi.com/?s=car&apikey=bb55fec8&page=1
+
+export async function agafarPelicules(pagina){
+    let data = "";
+
+    const response = await fetch(`data.JSON`)
+    const productes = await response.json();
+
+    return productes;
+
+    
+}
